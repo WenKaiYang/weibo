73be2fb76d561582c90ecd20ec44ed01229c428b
WenKaiYang <313097973@qq.com>
2020-06-27 22:31:44 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,6 +27,6 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-        
+
     }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,6 +13,7 @@
     @yield('content')
     @include('layouts._footer')
 
+    <script src="{{ mix('js/app.js') }}"></script>
 </div>
 </body>
 </html>
