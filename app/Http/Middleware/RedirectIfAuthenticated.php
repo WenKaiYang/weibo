df0273402a4964532796270b352e3080821bfd8c
WenKaiYang <313097973@qq.com>
2020-06-27 22:58:55 +0800

    访问策略

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,8 +18,13 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+//        if (Auth::guard($guard)->check()) {
+//            return redirect(RouteServiceProvider::HOME);
+//        }
+
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            session()->flash('info', '您已登录，无需再次操作。');
+            return redirect('/');
         }
 
         return $next($request);
